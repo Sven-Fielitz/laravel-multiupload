7f8a1f685ba1819084147f18e34972b11312b271
Sven <sven.fielitz@t-online.de>
2022-07-30 00:10:28 +0200

    Installieren von JQuery + Bootstrap und Dropzone.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,13 @@
 @extends('themes.default')
 
 @section('content')
-    Hallo Welt
+<div class="h-100 d-flex align-items-center justify-content-center">
+  <div>  
+    <form action="{{route('upload')}}"
+        class="dropzone"
+        id="my-awesome-dropzone">
+        @csrf
+    </form>
+  </div>
+</div>
 @endsection
--- resources/views/themes/default.blade.php
+++ resources/views/themes/default.blade.php
@@ -3,13 +3,22 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         @hasSection('title')
             @yield('title')
         @else
             <title>Multiupload</title>
         @endif
-      
+
+        <link rel="stylesheet" href="{{url('/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{url('/css/app.css')}}">
+        <link rel="stylesheet" href="{{url('/css/dropzone.min.css')}}">
+        <script language="javascript" type="text/javascript" src="{{url('/js/jquery-3.6.0.min.js')}}"></script>
+        <script language="javascript" type="text/javascript" src="{{url('/js/bootstrap.bundle.min.js')}}"></script>
+        <script language="javascript" type="text/javascript" src="{{url('/js/dropzone.min.js')}}"></script>
+        <script language="javascript" type="text/javascript" src="{{url('/js/app.js')}}"></script>
+
     </head>
     <body>
         <div class="header">
