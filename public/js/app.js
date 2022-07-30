a219254418100cfe1a553caf58dcb70da64d9cfd
Sven Fielitz <Sven.Fielitz@t-online.de>
2022-07-30 22:15:57 +0200

    Dateigröße prüfen und Fehlermeldungen ausgeben

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,6 @@ $( document ).ready(function() {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-    $('fileupload').dropzone({ url: "/file/post" });;
 
     $("#UploadedFiles").dataTable({
        
@@ -13,15 +12,33 @@ $( document ).ready(function() {
 
 });
 
+//Angabe in MB (x * 1.000.000 Byte)
+var maxFilesize = 5;
+
 Dropzone.options.fileupload =  {
+    accept: function(file, done) {
+        
+        //Da das Dateilimit bei 5 MB und nicht bei 5 MiB liegen soll, wird hier manuell geprüft.
+        //Die Option maxFilesize von Dropzone.js kann nur in MiB angegeben werden und ist daher zu ungenau.
+        
+        if(file.size <= maxFilesize * 1000 * 1000) {
+            done();
+        } else {
+            done("Die Datei ist zu groß. Die maximale Dateigröße beträgt "+maxFilesize+" MB");
+        }
+
+    },
+    sending: function(file, xhr , formData) {
+
+    },
+
     headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     },
-    'maxFilesize': 5,
-    'dictDefaultMessage': 'Legen Sie Dateien hier ab um Sie hochzuladen<br>(Maximal 5 MB)',
+    'acceptedFiles': '.jpg, .jpeg, .png, .gif, .pdf',
+    'dictDefaultMessage': 'Legen Sie Dateien hier ab um Sie hochzuladen<br>(Maximal ' + maxFilesize + ' MB - Erlaubt sind Bilder und PDFs)',
     'dictFallbackMessage': 'Ihr Browser unterstützt Drag&Drop Dateiuploads nicht',
     'dictFallbackText': 'Benutzen Sie das Formular um Ihre Dateien hochzuladen',
-    'dictFileTooBig': 'Die Datei ist zu groß. Die maximale Dateigröße beträgt {{maxFileSize}}MB',
     'dictInvalidFileType': 'Eine Datei dieses Typs kann nicht hochgeladen werden',
     'dictResponseError': 'Der Server hat ihre Anfrage mit Status {{statusCode}} abgelehnt',
     'dictCancelUpload': 'Hochladen abbrechen',
